a1c18dbd0f1887e2bff8f66501d4c43ff92fa080
carsehorse <email@example.com>
2023-01-30 13:09:36 +0200

    Git init

---
+++ src/main/java/com/home/textanalyzerappapi/TextAnalyzerAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.home.textanalyzerappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextAnalyzerAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/textanalyzerappapi/TextAnalyzerAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.textanalyzerappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzerAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
